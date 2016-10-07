00757f2e5c6632e66980327f6fb91734b5dbaa2f
LV <nikki.solution@gmail.com>
2016-10-07 17:19:58 +0500

    Update lulz.js

--- js/lulz.js
+++ js/lulz.js
@@ -1,1 +1,42 @@
+$(function() {
 
+    function buildHiddenMenu(elements) {
+        $(window).on('resize', function() {
+            if(!elements.navigationMenu.find(elements.hiddenMenu).length) {
+                elements.navigationMenu.append(elements.hiddenMenu);
+            }
+
+            elements.hiddenMenu.empty();
+
+            elements.wrapperList
+                .find('li').filter(function(index, element) {
+                    return $(element).position().top > 20;
+                })
+                .addClass('is-overflowed')
+                .clone()
+                .removeClass('is-overflowed')
+                .appendTo(elements.hiddenMenu);
+
+            if (!elements.hiddenMenu.find('li').length) {
+                $(elements.hiddenMenu).css({'display' : 'none'});
+                $(elements.triggerMenu).css({'display' : 'none'});
+            } else {
+                $(elements.triggerMenu).css({'display' : 'block'});
+            }
+
+            elements.triggerMenu.on('click', function(event) {
+                event.preventDefault();
+                elements.hiddenMenu.toggle();
+            });
+
+        }).trigger('resize');
+    }
+
+    buildHiddenMenu({
+        wrapperList: $('.wrapper-list'),
+        hiddenMenu: $('<ul class="hidden-list"/>'),
+        triggerMenu: $('.trigger-menu'),
+        navigationMenu: $('.navigation')
+    });
+    
+});
